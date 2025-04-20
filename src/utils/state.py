0bab5ce4304aa7c99ebf9f11a7e04153bdf0b921
Dustin <126636792+DustinRobertHofer@users.noreply.github.com>
2025-04-20 12:08:12 -0400

    First Commit

---
+++ src/utils/state.py
@@ -0,0 +1,69 @@
+from math import cos, sin, pi
+
+class State:
+    def __init__(self):
+        """Initialize state tracking system"""
+        self.x = 0.0  # X position in meters
+        self.y = 0.0  # Y position in meters
+        self.theta = 0.0  # Heading in radians
+        
+        # Parameters for turn detection
+        self.prev_theta = 0.0
+        self.TURN_THRESHOLD = 0.01  # About 0.57 degrees in radians
+        
+        # Obstacle detection parameters
+        self.obstacle_detected = False
+        self.obstacle_distance = float('inf')
+        self.OBSTACLE_THRESHOLD = 0.5  # Distance in meters to consider as obstacle
+        
+    def update(self, sensor_data):
+        """Update state estimation using sensor data"""
+        # Get sensor readings
+        wheel_data = sensor_data['wheel_data']
+        compass_heading = sensor_data['compass_heading']
+        
+        # Update heading
+        self.prev_theta = self.theta
+        self.theta = self.normalize_angle(compass_heading)
+        
+        # Check if turning
+        is_turning = abs(self.theta - self.prev_theta) > self.TURN_THRESHOLD
+        
+        # Only update position if not turning
+        if not is_turning and wheel_data['forward_motion'] != 0:
+            forward_motion = wheel_data['forward_motion']
+            self.x += forward_motion * cos(self.theta)
+            self.y += forward_motion * sin(self.theta)
+            
+        # Update obstacle detection
+        if 'distance' in sensor_data:
+            self.obstacle_distance = sensor_data['distance']
+            self.obstacle_detected = self.obstacle_distance < self.OBSTACLE_THRESHOLD
+        else:
+            self.obstacle_detected = False
+            self.obstacle_distance = float('inf')
+    
+    def normalize_angle(self, angle):
+        """Normalize angle to [-pi, pi]"""
+        if angle > pi:
+            angle -= 2 * pi
+        elif angle < -pi:
+            angle += 2 * pi
+        return angle
+            
+    def get_position(self):
+        """Return current estimated position and heading"""
+        return {
+            'x': self.x,
+            'y': self.y,
+            'theta': self.theta,
+            'obstacle_detected': self.obstacle_detected,
+            'obstacle_distance': self.obstacle_distance
+        }
+        
+    def reset(self):
+        """Reset position estimation to origin"""
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
+        self.prev_theta = 0.0 
